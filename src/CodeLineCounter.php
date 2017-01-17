2fa0c516f760ff3234e5c090631bbc1d4e44046b
wangxuan <wangxuan@qgpic.com>
2017-01-17 18:25:09 +0800

    update with add options

--- src/CodeLineCounter.php
+++ src/CodeLineCounter.php
@@ -4,6 +4,7 @@ namespace Fuguevit\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CodeLineCounter extends Command
@@ -11,14 +12,38 @@ class CodeLineCounter extends Command
     protected function configure()
     {
         $this->setName('count-line')
-            ->setDescription('Count code lines.');
+            ->setDescription('Count code lines.')
+            ->addOption('suffix', null, InputOption::VALUE_REQUIRED, 'Specify file suffix.')
+            ->addOption('exclude', null, InputOption::VALUE_REQUIRED, 'Specify exclude directory.');
     }
     
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $result = exec("find . -type f -execdir cat {} \;| wc -l ");
+        $fileFoundCommand = "find . -type f -execdir cat {} \;";
+        $wordCountCommand = "wc -l";
+        
+        // Add -name for find.
+        if ($suffix = $input->getOption('suffix')) {
+            $nameOption = "-name '*.$suffix'";
+            $fileFoundCommand = $this->str_replace_first('.', '. '.$nameOption, $fileFoundCommand);
+        }
+
+        // Add -exclude for filter directory.
+        if ($exclude = $input->getOption('exclude')) {
+            $excludeOption = "-path './$exclude' -prune -o";
+            $fileFoundCommand = $this->str_replace_first('.', '. '.$excludeOption, $fileFoundCommand);
+        }
+
+        $command = $fileFoundCommand.' | '.$wordCountCommand;
+        $result = exec($command);
         
         $output->writeln($result);
     }
 
+    protected function str_replace_first($from, $to, $subject)
+    {
+        $from = '/'.preg_quote($from, '/').'/';
+
+        return preg_replace($from, $to, $subject, 1);
+    }
 }
